9829201f0810f035667093cf1c574f5af7b9019f
Parag Chaudhari <paragmchaudhari@gmail.com>
2015-09-10 21:16:37 -0700

    adding the domain classes Dimension and Cube

---
+++ one/src/main/java/asu/edu/sd/spring/domain/Cube.java
@@ -0,0 +1,28 @@
+package asu.edu.sd.spring.domain;
+
+public class Cube {
+	
+	private double length;
+	private double volume;
+	private int unit;
+	
+	public double getLength() {
+		return length;
+	}
+	public void setLength(double length) {
+		this.length = length;
+	}
+	public double getVolume() {
+		return volume;
+	}
+	public void setVolume(double volume) {
+		this.volume = volume;
+	}
+	public int getUnit() {
+		return unit;
+	}
+	public void setUnit(int unit) {
+		this.unit = unit;
+	}
+	
+}
---
+++ one/src/main/java/asu/edu/sd/spring/domain/Dimension.java
@@ -0,0 +1,20 @@
+package asu.edu.sd.spring.domain;
+
+public class Dimension {
+	private double length;
+	private int unit;
+	
+	public double getLength() {
+		return length;
+	}
+	public void setLength(double length) {
+		this.length = length;
+	}
+	public int getUnit() {
+		return unit;
+	}
+	public void setUnit(int unit) {
+		this.unit = unit;
+	}
+	
+}
